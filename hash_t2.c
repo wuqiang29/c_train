8a065440938d9a0737e526a2a2bc7ef4c8484e22
wuqiang <wuqqiang@163.com>
2018-06-14 14:53:16 +0800

    hash_t2 finished

--- hash_t2.c
+++ hash_t2.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define OK 1
+#define ERROR 0
+
 #define NULLKEY 0
 #define EQ(a,b) ((a)==(b))
 
 #define success		1
 #define unsuccess	0
 #define duplicate	-1
+
+#define N 10
 
 typedef int status;
 typedef int keytype;
@@ -51,14 +56,22 @@ status Inithashtable(Hashtable *h)
 		h->elem[i].key=NULLKEY;
 	return OK;
 }
-
+/*销毁hash表*/
+void Destroyhashtable(Hashtable *h)
+{
+	free(h->elem);
+	h->elem=NULL;
+	h->count=0;
+	h->sizeindex=0;	
+}
 /*查找关键字k的元素*/
 status Searchhash(Hashtable h,keytype k,int *p,int *c)
 {
 	*p=Hash(k);
 	while(h.elem[*p].key!=NULLKEY&&!EQ(k,h.elem[*p].key))
 	{
-		(*c)++;
+		//printf("h.elem[*p].key!=NULLKEY p=[%d] k=[%d} key=[%d]\n",*p,k,h.elem[*p].key);
+		//(*c)++;
 		if(*c<m)
 			collision(p,*c);
 		else
@@ -70,7 +83,8 @@ status Searchhash(Hashtable h,keytype k,int *p,int *c)
 		return unsuccess;
 }
 
-status Inserthash(Hashtable *h, Elemtype e)
+status Inserthash(Hashtable *h, Elemtype e);
+
 void Recreatehashtable(Hashtable *h)
 {
 	int i,count=h->count;
@@ -84,7 +98,7 @@ void Recreatehashtable(Hashtable *h)
 	}
 	h->count=0;
 	h->sizeindex++;
-	m=hashsize(h->sizeindex);
+	m=hashsize[h->sizeindex];
 	p=(Elemtype*)realloc(h->elem,m*sizeof(Elemtype));
 	if(!p)
 		exit(OVERFLOW);
@@ -93,19 +107,20 @@ void Recreatehashtable(Hashtable *h)
 	for(i=count;i<m;i++)
 		h->elem[i].key=NULLKEY;
 	for(p=elem;p<elem+count;p++)
-		Inserthash(h,*p);				
-		
+		Inserthash(h,*p);						
 }
+/*查找不成功时插入*/
 status Inserthash(Hashtable *h, Elemtype e)
 {
 	int c,p;
 	c=0;
 	if(Searchhash(*h,e.key,&p,&c))
-		return duplicate
+		return duplicate;
 	else if(c<hashsize[(*h).sizeindex]/2)
 	{
-		h->elem[p]=e;
-		++(h*).count;
+		h->elem[p-1]=e;
+		printf("insert success key[%d] p[%d] ord[%d]\n",e.key,p-1,e.ord);
+		++(*h).count;
 		return OK;		
 	}
 	else
@@ -113,9 +128,42 @@ status Inserthash(Hashtable *h, Elemtype e)
 	return ERROR;
 }
 
-int mian(int argc, char* argv[])
+void print(int p,Elemtype r)
 {
-	Elemtype r[N]={{17,1},{60,2},{29,3},{38,4},{1,5},{2,6},{3,7},{4,8},{60,9},{13,10}};
+	printf("address = %d(%d,%d)\n",p,r.key,r.ord);
+}
+
+/*按哈希地址的顺序遍历哈希表*/
+void Traversehash(Hashtable h,void(*Vi)(int p, Elemtype r))
+{
+	int i;
+	printf("哈希地址0~%d\n",m-1);
+	for(i=0;i<m;i++)
+		if(h.elem->key!=NULLKEY)
+			Vi(i,h.elem[i]);
+}
+
+status Find(Hashtable h, keytype k,int *p)
+{
+	int c=0;
+	*p = Hash(k);
+	
+	while(h.elem[*p].key!=NULLKEY&&!EQ(k,h.elem[*p].key))
+	{
+		c++;
+		if(c<m)
+			collision(p,c);
+		else
+			return unsuccess;
+	}
+	if(EQ(k,h.elem[*p].key))
+		return success;
+	else
+		return unsuccess;
+}
+int main(int argc, char* argv[])
+{
+	Elemtype r[N]={{17,1},{60,2},{29,3},{38,4},{1,5},{2,6},{3,7},{4,8},{61,9},{13,10}};//,{10,9}};//,{21,9}};
 	Hashtable h;
 	int i,p;
 	status j;
@@ -127,6 +175,15 @@ int mian(int argc, char* argv[])
 		if(j==duplicate)
 			printf("表中已有关键字为%d的记录，无法在插入记录(%d,%d)\n",r[i].key,r[i].key,r[i].ord);		
 	}
+	printf("按哈希地址的顺序遍历哈希表：\n");
+	Traversehash(h,print);
 	
+	printf("请输入带查找记录的关键字：\n");
+	scanf("%d",&k);
 	
+	j = Find(h,k,&p);
+	if(j=success)
+		print(p,h.elem[p]);
+	else
+		printf("没有找到！\n");
 }
